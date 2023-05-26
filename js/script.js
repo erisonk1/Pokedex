ffe1087a7046d05ee89f225958a58d7b7955694d
Erison Kauã <erisonkauamkt@gmail.com>
2023-05-26 12:45:45 -0300

    dsadas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const pokeImage = document.querySelector('.poke-image');
 const pokeName = document.querySelector('.poke-name');
 const btn = document.querySelector('.passbtn');
+const containerMain = document.querySelector('.container-main');
+const info = document.querySelectorAll('.infos');
+
 var idx = 1
 function passarIDX() {
     idx++;
@@ -10,17 +13,21 @@ btn.addEventListener('click', passarIDX)
 
 
 
-
-
+function pagination(){
+    containerMain.innerHTML = containerMain.innerHTML + `<div class="infos">
+    <div class="back"><img class="poke-image" src="" alt=""></div>
+     <h3 class="poke-name"></h3>
+ </div>`;
+}
 
 function buscaPokemon(id){
 fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
 .then(r => r.json())
 .then(pokeSearch => {
     console.log(pokeSearch);
-    pokeImage.src = pokeSearch.sprites.front_default;
+    pokeImage.src = pokeSearch.sprites.other.dream_world.front_default;
     pokeName.innerText = pokeSearch.name;
 })
 }
-
+buscaPokemon(idx);
 console.log(btn)
