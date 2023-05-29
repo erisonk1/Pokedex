8966d40da396c17d01dcbad68830544fa6ec263b
Erison Kauã <erisonkauamkt@gmail.com>
2023-05-29 11:50:26 -0300

    tgfef

--- js/script.js
+++ js/script.js
@@ -1,33 +1,34 @@
-const pokeImage = document.querySelectorAll('.poke-image');
-const pokeName = document.querySelector('.poke-name');
-const btn = document.querySelector('.passbtn');
+const pokeImg = document.querySelector('.card-poke-img');
+const pokeName = document.querySelector('.card-poke-name');
 const containerMain = document.querySelector('.container-main');
-const info = document.querySelectorAll('.infos');
+const pokeId = document.querySelector('.card-poke-id');
+const card = document.querySelector('card');
 
-var idx = 1
-function passarIDX() {
-    idx++;
-    buscaPokemon(idx);
-}
-btn.addEventListener('click', passarIDX)
-
-
-
-function pagination(){
-    containerMain.innerHTML = containerMain.innerHTML + `<div class="infos">
-    <div class="back"><img class="poke-image" src="" alt=""></div>
-     <h3 class="poke-name"></h3>
- </div>`;
-}
-
-function buscaPokemon(id){
-fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+let i = 1;
+fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
 .then(r => r.json())
-.then(pokeSearch => {
-    console.log(pokeSearch);
-    pokeImage.src = pokeSearch.sprites.other.dream_world.front_default;
-    pokeName.innerText = pokeSearch.name;
+.then(data => {
+    console.log(data)
+    pokeImg.src = data.sprites.other.dream_world.front_default;
+    pokeName.innerText = data.name
 })
+
+function criaCard() {
+    const mcard = document.createElement('div');
+    containerMain.appendChild(mcard)
+    mcard.classList.add('card');
+    mcard.innerHTML = `
+    <div class="card">
+                <div class="card-center">
+                 <div class="card-poke-back">
+                     <img class="card-poke-img" src="./img/charizard.svg" alt="">
+                 </div>
+                </div> 
+                
+                 <p class="card-poke-id">#006</p>
+                 <div class="card-center-stats"><h1 class="card-poke-name">Charizard </h1> <img class="card-poke-type" src="./img/fire.svg" alt=""></div>
+                 
+                 
+             </div>
+    `
 }
-buscaPokemon(idx);
-console.log(btn)
