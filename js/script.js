8a472ed7ff5cbfdfc01c2dc2681a47a50959a3a7
Erison Kauã <erisonkauamkt@gmail.com>
2023-06-19 13:16:35 -0300

    filtro
    filtro

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let pokeImg = document.querySelector(".card-poke-img");
 const pokeName = document.querySelectorAll(".card-poke-name");
 const containerMain = document.querySelector(".container-main");
-
+const stats = document.querySelectorAll(".num");
 const fixModal = document.querySelector(".fix-modal");
 const modal = document.querySelector(".modal");
 const modalClose = document.querySelector(".modal-close");
@@ -13,10 +13,22 @@ let pokeTipo;
 let containerType = document.querySelector(".modal-type-name");
 let idPoke;
 let span;
+let cardPokeImg;
+const search = document.querySelector(".search")
 
-function creatModal() {
-  const url = fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
-}
+function FilterCardSingleTipo(i) {
+  const urlType = fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+  Promise.resolve(urlType)
+    .then((response) => response.json())
+    .then((response) => {
+      pokeTipo = response.types[0].type.name;
+      console.log(pokeTipo)
+      const pokeTipoImg = document.querySelectorAll(".card-poke-type");
+
+      pokeTipoImg.src = `./img/icon-types/${pokeTipo}.svg`;
+      cardBtn = document.querySelectorAll(".cardBtn");
+      // PokeInd.src = `./img/icon-types/${pokeTipo}.svg`
+    })}
 
 function CardSingleTipo(i) {
   const urlType = fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
@@ -52,7 +64,7 @@ function fetchSingleApi(ind) {
     .finally();
 }
 
-function fetchApi() {
+function fetchApi(i) {
   const url = fetch(
     `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${ind - 1}`
   );
@@ -67,7 +79,7 @@ function fetchApi() {
         var urlFoto = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokeId}.svg`;
         var urlFoto2 = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeId}.png`;
         fetchSingleApi(ind);
-        criaCard(urlFoto, pokemon.name, pokeId, urlFoto2, pokeTipo);
+        criaCard(urlFoto, pokemon.name, pokeId, urlFoto2);
         CardSingleTipo(ind);
         ind++;
       });
@@ -130,15 +142,17 @@ fetchApi();
 
 function handleEvent(event) {
   fixModal.classList.add("active");
+  console.log(event)
   modal.classList.add("active");
   idPoke = event.srcElement;
-  // console.log(idPoke.id)
+  
+  
   function useModal() {
     const url = fetch(`https://pokeapi.co/api/v2/pokemon/${idPoke.id}/`);
     Promise.resolve(url)
       .then((url) => url.json())
       .then((url) => {
-        const modalImg = document.querySelector(".modal-img-poke");
+        let modalImg = document.querySelector(".modal-img-poke");
         const modalName = document.querySelector(".modal-poke-name");
         const cardImg = document.querySelectorAll(".card-poke-img");
         const modalId = document.querySelector(".modal-poke-id");
@@ -152,36 +166,39 @@ function handleEvent(event) {
         modalWeight.innerText = url.weight / 10 + "kg";
         modalAbilities.innerText = url.abilities[0].ability.name;
         function typeModal(url) {
-          span = document.querySelectorAll('.modal-type-name > span')
-          console.log(span)
+          span = document.querySelectorAll(".modal-type-name > span");
+          console.log(span);
           url.types.map((type, index) => {
-              console.log(type.type.name)
-              span[index].innerHTML = type.type.name
-              span[index].style.color = `var(--${type.type.name})`
-              span[index].style.backgroundColor = `var(--bg-${type.type.name})`
-          })
+            console.log(type.type.name);
+            span[index].innerHTML = type.type.name;
+            span[index].style.color = `var(--${type.type.name})`;
+            span[index].style.backgroundColor = `var(--bg-${type.type.name})`;
+          });
         }
+        url.stats.map((pokeStatus, index) => {
+          stats[index].innerText = pokeStatus.base_stat;
+          cardPokeImg = document.querySelector('.card-poke-img')
 
+        });
 
-        // for (i = 0; i < bgLine.length; i++) {
-        //   bgLine[i].style.width = +url.stats[i].base_stat / 1.6 + "%";
-        // }
-        // url.stats.map((status, index) => {
-        //   bgLine[index].style.width = status.base_stat / 1.6 + "%";
-        // })
 
         console.log(pokeType);
         modalBg.style.backgroundImage = `url('./img/bg-types/${pokeType}.svg')`;
         console.log(modalBg.style);
         modalId.innerText = "#" + url.id;
-        // console.log(url.name)
+        
+        cardPokeImg = document.querySelector('.card-poke-img')
+
         modalName.innerText = url.name;
         // console.log(cardImg)
-        modalImg.src = cardImg[idPoke.id - 1].src;
-        typeModal(url);
-      })
-      .then((url) => {
+        if(cardImg.length == 1) {
+          modalImg.src = cardPokeImg.src;
+        }
+        else{
+          modalImg.src = cardImg[idPoke.id - 1].src;
+        }
 
+        typeModal(url);
       });
   }
   useModal(idPoke.id);
@@ -202,10 +219,44 @@ function changeCategory(idPoke) {
 function close() {
   modal.classList.remove("active");
   fixModal.classList.remove("active");
-  span[1].innerHTML = ''
-  span[1].style.backgroundColor = 'white'
+  span[1].innerHTML = "";
+  span[1].style.backgroundColor = "white";
 }
 
+function filter() {
+  const url = fetch(`https://pokeapi.co/api/v2/pokemon/${search.value}/`)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response)
+    console.log(search.value)
+  const pokeId = response.id
+  pokeTipo = response.types[0].type.name;
+  containerMain.innerHTML = ''
+  const urlFoto = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokeId}.svg`
+  const urlFoto2 = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeId}.png`
+    console.log(pokeId, urlFoto, response.name, urlFoto2,pokeTipo)
+  criaCard(urlFoto, response.name, pokeId, urlFoto2);
+  pokeId
+  function aaaa(response) {
+    pokeTipo = response.types[0].type.name;
+    const pokeBack = document.querySelector(".card-poke-back");
+    pokeBack.style.backgroundColor = `var(--${pokeTipo})`;
+    const CardType = document.querySelector('.card-poke-type')
+    CardType.src = `./img/icon-types/${pokeTipo}.svg`
+    cardPokeImg = document.querySelector('.card-poke-img')
+  }
+aaaa(response)
 
+  }).then(()=> {
+    FilterCardSingleTipo(search.value)
+    console.log('aaaaaaa')
+  }) 
+    
 
+}
+
+  search.addEventListener('keyup', (event) => {
+    if (event.code == "Enter") {
+      filter();
+    }});
 modalClose.addEventListener("click", close);
