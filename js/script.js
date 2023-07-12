e4c764ac1dc67c7aaeaac448d8708b6d8f10baa2
Erison Kauã <erisonkauamkt@gmail.com>
2023-07-12 10:01:31 -0300

    update

--- js/script.js
+++ js/script.js
@@ -14,13 +14,13 @@ let containerType = document.querySelector(".modal-type-name");
 let idPoke;
 let spanType;
 let cardPokeImg;
-const search = document.querySelector(".search")
+const search = document.querySelector(".search");
 let pokeWeak;
-let modalWeak = document.querySelector('.modal-weak-info')
-const load = document.querySelector('.load')
-const lista = document.querySelectorAll('li > a');
+let modalWeak = document.querySelector(".modal-weak-info");
+const load = document.querySelector(".load");
+const lista = document.querySelectorAll("li > a");
 let pokeIdImg;
-const loading = document.querySelector('.loading')
+const loading = document.querySelector(".loading");
 
 function FilterCardSingleTipo(i) {
   const urlType = fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
@@ -28,13 +28,13 @@ function FilterCardSingleTipo(i) {
     .then((response) => response.json())
     .then((response) => {
       pokeTipo = response.types[0].type.name;
-      console.log(pokeTipo)
       const pokeTipoImg = document.querySelectorAll(".card-poke-type");
 
       pokeTipoImg.src = `./img/icon-types/${pokeTipo}.svg`;
       cardBtn = document.querySelectorAll(".cardBtn");
       // PokeInd.src = `./img/icon-types/${pokeTipo}.svg`
-    })}
+    });
+}
 
 function CardSingleTipo(i) {
   const urlType = fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
@@ -71,7 +71,7 @@ function fetchSingleApi(ind) {
 }
 
 function fetchApi(i) {
-  loading.style.display = "flex"
+  loading.style.display = "flex";
   const url = fetch(
     `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${ind - 1}`
   );
@@ -91,9 +91,8 @@ function fetchApi(i) {
         ind++;
       });
       i = i + 20;
-    }).then(() =>   
-    setTimeout(() => loading.style.display = "none"))
-    
+    })
+    .then(() => setTimeout(() => (loading.style.display = "none")))
 
     .catch((erro) => console.log("deu erro/nao carregouuuuuuuuuu", erro))
     .finally();
@@ -151,89 +150,76 @@ fetchApi();
 
 function openCard(event) {
   fixModal.classList.add("active");
-  console.log(event)
   modal.classList.add("active");
   idPoke = event.srcElement;
-  img = event.srcElement.offsetParent.childNodes[1].firstElementChild.firstElementChild.firstElementChild
-  
+  img =
+    event.srcElement.offsetParent.childNodes[1].firstElementChild
+      .firstElementChild.firstElementChild;
+
   function useModal(event) {
     const url = fetch(`https://pokeapi.co/api/v2/pokemon/${idPoke.id}/`);
     Promise.resolve(url)
       .then((url) => url.json())
-      .then((url) => {        let modalImg = document.querySelector(".modal-img-poke");
+      .then((url) => {
+        let modalImg = document.querySelector(".modal-img-poke");
         const modalName = document.querySelector(".modal-poke-name");
         const cardImg = document.querySelectorAll(".card-poke-img");
         const modalId = document.querySelector(".modal-poke-id");
         const modalBg = document.querySelector(".modal-bg-type");
-        pokeWeak = url.types[0].type.url
-        console.log(pokeWeak)
+        pokeWeak = url.types[0].type.url;
         const pokeType = url.types[0].type.name;
         const bgLine = document.querySelectorAll(".line-bg");
         const modalHeight = document.querySelector(".modal-height > p");
         const modalWeight = document.querySelector(".modal-weight > p");
         const modalAbilities = document.querySelector(".modal-abilities > p");
         const typeIcon = document.querySelector(".modal-type-icon");
-        typeIcon.src = `./img/icon-types/${pokeType}.svg`
+        typeIcon.src = `./img/icon-types/${pokeType}.svg`;
         modalHeight.innerText = url.height / 10 + "m";
         modalWeight.innerText = url.weight / 10 + "kg";
         modalAbilities.innerText = url.abilities[0].ability.name;
-        console.log(url.types)
 
         function typeModal(url) {
           spanType = document.querySelectorAll(".modal-type-name > span");
-          console.log(spanType);
-          console.log(url.types)
+
 
           url.types.map((type, index) => {
-            console.log(type.type.name);
             spanType[index].innerHTML = type.type.name;
             spanType[index].style.color = `var(--${type.type.name})`;
-            spanType[index].style.backgroundColor = `var(--bg-${type.type.name})`;
-            
+            spanType[
+              index
+            ].style.backgroundColor = `var(--bg-${type.type.name})`;
           });
-          
         }
         url.stats.map((pokeStatus, index) => {
           stats[index].innerText = pokeStatus.base_stat;
-          cardPokeImg = document.querySelector('.card-poke-img')
-
+          cardPokeImg = document.querySelector(".card-poke-img");
         });
 
-
-        console.log(pokeType);
         modalBg.style.backgroundImage = `url('./img/bg-types/${pokeType}.svg')`;
-        console.log(modalBg.style);
         modalId.innerText = "#" + url.id;
-        
-        
 
         modalName.innerText = url.name;
-        // console.log(cardImg)
         function mudaImagem() {
-          console.log(img)
-          modalImg.src = img.src
-          cardPokeImg = document.querySelectorAll('.card-poke-img')
-          console.log(cardPokeImg)
+          modalImg.src = img.src;
+          cardPokeImg = document.querySelectorAll(".card-poke-img");
         }
-        mudaImagem()
+        mudaImagem();
         typeModal(url);
-      function weak(pokeWeak) {
-        const url = fetch(`${pokeWeak}`)
-        .then((r) => r.json())
-        .then((r) => {
-          console.log(r)
-          const weak = r;
-          weak.damage_relations.double_damage_from.map((weak, index) => {
-            console.log(weak)
-            const spanWeak = document.createElement("span");
-            modalWeak.appendChild(spanWeak)
-            spanWeak.innerText = `${weak.name}`;
-            spanWeak.style.color = `var(--${weak.name})`;
-            spanWeak.style.backgroundColor = `var(--bg-${weak.name})`;
-          })
-        })
-      }
-      weak(pokeWeak)
+        function weak(pokeWeak) {
+          const url = fetch(`${pokeWeak}`)
+            .then((r) => r.json())
+            .then((r) => {
+              const weak = r;
+              weak.damage_relations.double_damage_from.map((weak, index) => {
+                const spanWeak = document.createElement("span");
+                modalWeak.appendChild(spanWeak);
+                spanWeak.innerText = `${weak.name}`;
+                spanWeak.style.color = `var(--${weak.name})`;
+                spanWeak.style.backgroundColor = `var(--bg-${weak.name})`;
+              });
+            });
+        }
+        weak(pokeWeak);
       });
   }
   useModal(idPoke.id);
@@ -255,141 +241,141 @@ function close() {
   spanType[1].innerHTML = "";
   spanType[1].style.backgroundColor = "transparent";
   modalWeak.innerHTML = "";
-
 }
 
 function filter() {
-  containerMain.innerHTML = ''
-  url = fetch(`https://pokeapi.co/api/v2/pokemon/${search.value.toLowerCase()}/`)
-  .then((response) => response.json())
-  .then((response) => {
-    console.log(response)
-    console.log(search.value)
-  const pokeId = response.id
-  pokeTipo = response.types[0].type.name;
-  containerMain.innerHTML = ''
-  const urlFoto = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokeId}.svg`
-  const urlFoto2 = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeId}.png`
-    console.log(pokeId, urlFoto, response.name, urlFoto2,pokeTipo)
-  criaCard(urlFoto, response.name, pokeId, urlFoto2);
+  containerMain.innerHTML = "";
+  url = fetch(
+    `https://pokeapi.co/api/v2/pokemon/${search.value.toLowerCase()}/`
+  )
+    .then((response) => response.json())
+    .then((response) => {
 
-  function aaaa(response) {
-    pokeTipo = response.types[0].type.name;
-    const pokeBack = document.querySelector(".card-poke-back");
-    pokeBack.style.backgroundColor = `var(--${pokeTipo})`;
-    const CardType = document.querySelector('.card-poke-type')
-    CardType.src = `./img/icon-types/${pokeTipo}.svg`
-    cardPokeImg = document.querySelector('.card-poke-img')
-    load.style.display = "none"
-  }
-aaaa(response)
+      const pokeId = response.id;
+      pokeTipo = response.types[0].type.name;
+      containerMain.innerHTML = "";
+      const urlFoto = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokeId}.svg`;
+      const urlFoto2 = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeId}.png`;
+      criaCard(urlFoto, response.name, pokeId, urlFoto2);
 
-  }).then(()=> {
-    FilterCardSingleTipo(search.value.toLowerCase())
-    console.log('aaaaaaa')
-
-  }) 
-    
-
+      function aaaa(response) {
+        pokeTipo = response.types[0].type.name;
+        const pokeBack = document.querySelector(".card-poke-back");
+        pokeBack.style.backgroundColor = `var(--${pokeTipo})`;
+        const CardType = document.querySelector(".card-poke-type");
+        CardType.src = `./img/icon-types/${pokeTipo}.svg`;
+        cardPokeImg = document.querySelector(".card-poke-img");
+        load.style.display = "none";
+      }
+      aaaa(response);
+    })
+    .then(() => {
+      FilterCardSingleTipo(search.value.toLowerCase());
+    });
 }
 
-  search.addEventListener('keyup', (event) => {
-    if (event.code == "Enter") {
-      filter();
-    }});
+search.addEventListener("keyup", (event) => {
+  if (event.code == "Enter") {
+    filter();
+  }
+});
 modalClose.addEventListener("click", close);
 
 var typeName;
 
-lista.forEach((el) => 
-el.addEventListener('click', (event) => {
-  typeName = el.innerText.toLowerCase()
-})
+lista.forEach((el) =>
+  el.addEventListener("click", (event) => {
+    typeName = el.innerText.toLowerCase();
+  })
 );
 
-lista.forEach((el) => 
-el.addEventListener('click', filterByType)
-);
-
-function filterByType() {
-  console.log(typeName)
-
-  filterType()
-  lista[0].addEventListener('click', all)
-   function filterType() {
-    loading.style.display = "flex"
-    const url = fetch(`https://pokeapi.co/api/v2/type/${typeName}`)
+lista.forEach((el) => el.addEventListener("click", filterByType));
+function filterType() {
+  loading.style.display = "flex";
+  const url = fetch(`https://pokeapi.co/api/v2/type/${typeName}`)
     .then((r) => r.json())
-    .then( (r)  => {
-      console.log(r.pokemon)
-      containerMain.innerHTML = '';
+    .then((r) => {
+      containerMain.innerHTML = "";
       r.pokemon.map((pokemon) => {
-        const id = pokemon.pokemon.url.split('/')[6]
+        const id = pokemon.pokemon.url.split("/")[6];
         fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-        .then((r) => r.json())
-        .then(async (r) => {
-          console.log(r)
-          const urlFoto = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
-          const urlFoto2 = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
-          
-           criaCard(urlFoto, r.name, id, urlFoto2);
-          changeBg()
-          i = 0
-          ind = 1
-          await escondeLoading()
-        })
-      })
+          .then((r) => r.json())
+          .then(async (r) => {
+            const urlFoto = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
+            const urlFoto2 = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
 
-    }).then(() => setTimeout(() => loading.style.display = "none", 20000))
-  }
-  
-
-function escondeLoading(){
-  loading.style.display = "none"
+            criaCard(urlFoto, r.name, id, urlFoto2);
+            changeBg();
+            i = 0;
+            ind = 1;
+            escondeLoading();
+          });
+      });
+    })
+    .then(() => setTimeout(() => (loading.style.display = "none"), 20000));
 }
+function changeBg() {
+  const pokeBack = document.querySelectorAll(".card-poke-back");
+  pokeBack.forEach((pb) => {
+    pb.style.backgroundColor = `var(--${typeName})`;
+  });
+  const CardType = document.querySelectorAll(".card-poke-type");
+  CardType.forEach((ct) => {
+    ct.src = `./img/icon-types/${typeName}.svg`;
+    load.style.display = "none";
+  });
+}
+function escondeLoading() {
+  loading.style.display = "none";
+}
+function filterByType() {
 
-    lista[0].removeEventListener('click', filterType)
 
-  
-  function changeBg() {
-    const pokeBack = document.querySelectorAll(".card-poke-back");
-    pokeBack.forEach((pb) => {
-      pb.style.backgroundColor = `var(--${typeName})`;
-    })
-    const CardType = document.querySelectorAll('.card-poke-type')
-    CardType.forEach((ct) => {
-      ct.src = `./img/icon-types/${typeName}.svg`
-    load.style.display = "none"
-    })
-    
-  }
+  filterType();
+  lista[0].addEventListener("click", all);
 
+
+
+  lista[0].removeEventListener("click", filterType);
+
+ 
 }
 
 function all() {
-  containerMain.innerHTML = ""
-  i = 0
-  ind = 1
-  pokeTipo
-  containerMain.innerHTML = ""
-  fetchApi()
-  i = 0
-  ind = 1
-  load.style.display = "block"
+  containerMain.innerHTML = "";
+  i = 0;
+  ind = 1;
+  pokeTipo;
+  containerMain.innerHTML = "";
+  fetchApi();
+  i = 0;
+  ind = 1;
+  load.style.display = "block";
 }
 
-filterByType()
 // lista.forEach((li) => {
 // li.addEventListener('click', filterByType)
 // })
 
+const imgFilter = document.querySelector(".open-nav > img");
+const btnFilter = document.querySelector(".open-nav");
 
-const imgFilter = document.querySelector('.open-nav > img')
-const btnFilter = document.querySelector('.open-nav')
-
-const nav = document.querySelector('nav')
+const nav = document.querySelector("nav");
 function openNav() {
-  nav.classList.toggle('active')
-  imgFilter.classList.toggle('active')
-  btnFilter.classList.toggle('active')
+  nav.classList.toggle("active");
+  imgFilter.classList.toggle("agctive");
+  btnFilter.classList.toggle("active");
 }
+
+const btnTop = document.querySelector(".btn-top");
+
+function voltarTopo() {
+  console.log(window.scrollY);
+  if (window.scrollY >= 1581) {
+    btnTop.classList.add("visible");
+  } else {
+    btnTop.classList.remove("visible");
+  }
+}
+
+window.addEventListener("scroll", voltarTopo);
